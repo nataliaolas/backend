e27a1f66b4436b8d4cf73795e889e38e236b2001
Natalia Olas <nataliaolas7@gmail.com>
2020-12-18 19:25:32 +0100

    naprawazdjec

--- jedzonko/jedzonko/urls.py
+++ jedzonko/jedzonko/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 from rest_framework import routers, serializers
 from jedzenie.views import RestauracjaView,TypRestauracjiView,WlascicielView,AdresView,KlientlView,PozycjaView,MenuView,OpiniaORestauracjiView,ZamowienieView
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register(r'restauracja', RestauracjaView)
@@ -33,4 +34,4 @@ router.register(r'zamowienie', ZamowienieView)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
